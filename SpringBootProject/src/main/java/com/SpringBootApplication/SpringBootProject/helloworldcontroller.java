c6b49f1e78499ebe7805aa458e2b04aca638817e
Sachey-25 <42082775+Sachey-25@users.noreply.github.com>
2021-09-06 16:20:19 +0530

    Add files via upload

---
+++ SpringBootProject/src/main/java/com/SpringBootApplication/SpringBootProject/helloworldcontroller.java
@@ -0,0 +1,13 @@
+package com.SpringBootApplication.SpringBootProject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworldcontroller {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello SpringBoot";
+	}
+}
